d344099c6d991efc581b3df02ca4084cde862d7b
Jahaziel <jahaziel.vaz@gmail.com>
2019-10-12 13:12:31 -0600

    Callbacks are now successfully executed

---
+++ callBacks/callBacks.js
@@ -0,0 +1,43 @@
+$(() => {
+  let display = $("#output");
+  let button = $("#btn");
+
+  const setText = (text) => {
+    $(display).text(text)
+  }
+
+  const checkAuth = (cb) => {
+    setText('Checking Auth...')
+    setTimeout(() => {
+      cb(true)
+    }, 2000)
+  }
+
+  const checkUser = (usr) => {
+    setText('Checking User...')
+    setTimeout(() => {
+      usr(true)
+    }, 2000)
+  }
+
+
+
+  $(button).on('click', () => {
+    checkAuth((auth) => {
+      if(auth){
+        checkUser((us) => {
+          if(us){
+            let user = {
+              username: 'Jazzy'
+            }
+
+            setText(user.username)
+          }
+        })
+      }
+    })
+  })
+
+
+
+}) //End of jQuery
--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Section 1 Answer Question
+$(() => {
+  let answerPromise = new Promise((resolve, reject) => {
+    if($('#input1').val() === 'yes'){
+      let myOutput = $('#output1').html('<img src="https://i.ytimg.com/vi/yXr3z7fOa7A/hqdefault.jpg"></img>')
+
+      resolve(myOutput)
+    } else {
+      let accessDenied = new Error('Access Denied')
+
+      reject(accessDenied)
+    }
+
+  })
+
+  let promiseResolution = () => {
+    answerPromise
+    .then((res) => {
+      $('#output1').text('It worked')
+      $('#output1').html('<img src="https://i.ytimg.com/vi/yXr3z7fOa7A/hqdefault.jpg"></img>')
+    })
+    .catch((err) => {
+      $('#output1').text('Program Broke')
+      console.log(err.message)
+    })
+  }
+
+  promiseResolution()
+
+
+
+}) // End of jQuery
